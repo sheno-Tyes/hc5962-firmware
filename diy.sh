18cffc2caa7a4e567593fd57c66dc171eff1b3c6
sheno-Tyes <42202881+sheno-Tyes@users.noreply.github.com>
2020-04-18 21:42:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+#mkdir package/base-files/files/config
+#echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
